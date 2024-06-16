381196ae9d7c4c5430594271562e21254680e0f6
Cameron Roudebush <roudebushcameron@gmail.com>
2024-06-15 20:55:55 -0400

    Updating deployments to include pushing

--- scripts/build.docker.ts
+++ scripts/build.docker.ts
@@ -9,12 +9,20 @@ export module DockerBuilder {
     console.log("[Sprout Docker Builder]", ...message);
   }
 
-  /** Executes the commands to build the docker container */
+  /**
+   * Executes the commands to build the docker container
+   *
+   * Additional tags can be added as arguments to this script.
+   *
+   * Like: `npm run build:docker -t croudebush/sprout:dev`
+   */
   export async function build() {
+    const additionalTags = process.argv.slice(2);
     const version = BackendBuilder.getGitVersion();
     const rootDir = path.join(__dirname, "..");
     log(`Building docker image for ${version}...`);
-    const command = `docker build -f dockerfile -t sprout:${version} .`;
+    if (additionalTags.length > 0) log(`Including additional tags ${additionalTags.join(", ")}`);
+    const command = `docker build -f dockerfile -t sprout:${version}${additionalTags.map((x) => ` -t ${x}`)} .`;
     await BackendBuilder.spawnProcess(command, rootDir);
     log("Build complete!");
   }
