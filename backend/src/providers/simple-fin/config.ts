3d6e28047636f52aa201cfd2d5190c4101d61813
Cameron Roudebush <roudebushcameron@gmail.com>
2026-05-03 10:58:12 -0400

    fix: Small bugfixes - SimpleFin is going back to updating once a day - Fixing a bug where widget provider would try to populate on web - Fixing transaction edit not working in private mode. We've disabled the amount validation for now because it's not editable anyways (the backend will never let it change) - Fixing a bug where transactions_filter always thought the initial state was in filter mode

--- backend/src/providers/simple-fin/config.ts
+++ backend/src/providers/simple-fin/config.ts
@@ -4,7 +4,7 @@ import { BaseProviderConfig } from "@backend/providers/base/config";
 /** The configuration for the SimpleFIN provider */
 export class SimpleFINConfig extends BaseProviderConfig {
   @ConfigurationMetadata.assign({ comment: "How often to update this provider." })
-  override syncFrequency: string = "0 8,20 * * *";
+  override syncFrequency: string = "0 8 * * *";
 
   @ConfigurationMetadata.assign({ comment: "How many API calls we allow per day, per user, for this provider." })
   rateLimit: number = 24;
